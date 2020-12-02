38f25450059ae906fa4888ecdecbf50259a07888
dustinklo <dustinlo.pcc@gmail.com>
2020-12-01 21:51:43 -0800

    added endpoint to test out subprocess bash cmd

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import time
 import json
 import uuid
 
+import subprocess
+
 from pprint import pprint
 
 from flask import Flask, Response, request, render_template, jsonify
@@ -18,22 +20,38 @@ def generate_event_source_data(d):
 	return 'data: %s\n\n' % json_str
 
 
-def event_stream():
-	for i in range(40):
-		_id = uuid.uuid4()
-		time.sleep(random.uniform(0, 0.2))
-		data = {
-			'id': str(_id),
-			'message': 'Data pushed from server: (%d)' % (i + 1),
-		}
-		app.logger.debug(data)
-		yield generate_event_source_data(data)
-
-
 @app.route('/stream')
 def stream():
+	def event_stream():
+		for i in range(40):
+			_id = uuid.uuid4()
+			time.sleep(random.uniform(0, 0.2))
+			data = {
+				'id': str(_id),
+				'message': 'Data pushed from server: (%d)' % (i + 1),
+			}
+			app.logger.debug(data)
+			yield generate_event_source_data(data)
+
 	app.logger.info(request.args)
 	resp = Response(event_stream(), mimetype="text/event-stream")
+	resp.headers['Cache-Control'] = 'no-cache'
+	resp.headers["Access-Control-Allow-Origin"] = "*"
+	resp.headers['Content-Type'] = 'text/event-stream'
+	return resp
+
+
+@app.route('/stdout')
+def stream_bash_stdout():
+	def execute():
+		cmd = 'for i in {1..10}; do echo "count: $i"; sleep 0.5; done;'
+		popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+		for stdout_line in iter(popen.stdout.readline, ""):
+			app.logger.debug(stdout_line)
+			yield 'data: ' + stdout_line + '\n'
+		popen.stdout.close()
+
+	resp = Response(execute(), mimetype="text/event-stream")
 	resp.headers['Cache-Control'] = 'no-cache'
 	resp.headers["Access-Control-Allow-Origin"] = "*"
 	resp.headers['Content-Type'] = 'text/event-stream'
